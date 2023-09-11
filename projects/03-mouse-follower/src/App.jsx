f03473d688d50c1dcfd1c9739418902181c924c6
davidpacco <david.j.pacco@gmail.com>
2023-09-11 10:44:24 -0500

    Add project #3

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,8 +1,39 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+  const followerStyle = {
+    transform: `translate(${position.x}px, ${position.y}px)`,
+    display: `${enabled ? 'block' : 'none'}`
+  }
+
+  useEffect(() => {
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+
+  }, [enabled])
+
+
   return (
-    <h1>Mouse follower</h1>
+    <main>
+      <div className="follower" style={followerStyle}></div>
+      <h2>Mouse follower</h2>
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Deactivate' : 'Activate'}
+      </button>
+    </main>
   )
 }
 
